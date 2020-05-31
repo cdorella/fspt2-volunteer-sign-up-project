8054d7bcbe0201991600d08b07876cd8a7bcb510
cdorella <caroldorella@gmail.com>
2020-05-31 21:32:38 +0200

    boilerplate client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,48 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
 import "./App.css";
 
+import Admin from "./pages/admin-page";
+import HomePage from "./pages/home-page";
+import About from "./pages/about-page";
+
 class App extends React.Component {
-	state = {};
+	state = {
+		path: "/",
+	};
+
+	handleOnClick = path => {
+		this.setState({ path });
+	};
 
 	render() {
 		return (
 			<div className="App">
-				<h1>Hello World</h1>
+				<h1>Esperança Project</h1>
+				<BrowserRouter>
+					<ul>
+						<li>
+							<NavLink exact activeClassName="active-item" to="/">
+								Home Page
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeClassName="active-item" to="/admin">
+								Admin
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeClassName="active-item" to="/about">
+								About Us
+							</NavLink>
+						</li>
+					</ul>
+					<Switch>
+						<Route exact path="/" children={HomePage} />
+						<Route path="/admin" children={Admin} />
+						<Route path="/about" children={About} />
+					</Switch>
+				</BrowserRouter>
 			</div>
 		);
 	}
