9db2ee68d75ee3bedd31c5d9ea53e1c2683956de
cdorella <caroldorella@gmail.com>
2020-08-04 17:39:25 +0200

    fix: script

--- server/app.js
+++ server/app.js
@@ -2,20 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
+const clientPath = path.join(__dirname, "../", "client", "build");
 const apiRoutes = require("./api");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(clientPath));
 
 app.use("/api", apiRoutes);
 
-app.get("/", (req, res) => {
-	res.send({
-		message: "Hello World",
-	});
+// app.get("/", (req, res) => {
+// 	res.send({
+// 		message: "Hello World",
+// 	});
+// });
+
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(clientPath, "index.html"));
 });
 
 app.listen(process.env.PORT, () => {
