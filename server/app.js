4286748a5f5abe97033b2018dab95cd373eb23f4
cdorella <caroldorella@gmail.com>
2020-08-04 16:52:53 +0200

    fix: script files

--- server/app.js
+++ server/app.js
@@ -2,22 +2,29 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
+const clientPath = path.join(__dirname, "../", "client", "build");
 const apiRoutes = require("./api");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(clientPath));
 
 app.use("/api", apiRoutes);
 
-app.get("/", (req, res) => {
-	res.send({
-		message: "Hello World",
-	});
+// app.get("/", (req, res) => {
+// 	res.send({
+// 		message: "Hello World",
+// 	});
+// });
+
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(clientPath, "index.html"));
 });
 
-app.listen(process.env.API_PORT, () => {
-	console.log(`Starting server in PORT ${process.env.API_PORT}`);
+app.listen(process.env.PORT, () => {
+	console.log(`Starting server in PORT ${process.env.PORT}`);
 });
